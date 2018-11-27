b2b4ffb6f5705a2b8158075a9bdae6ed38bc692f
gongdapeng <gongdapeng@bytedance.com>
2018-11-27 22:32:06 +0800

    feat(init): first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# the super tiny compiler in TypeScript
+
+Refer to [the-super-tiny-compiler](https://github.com/jamiebuilds/the-super-tiny-compiler)，use Typescript to rewrite. A example for learning TypeScript.
+
+## Install
+
+`npm i`
+
+## Build
+
+`npm run build`
+
+## Test
+
+`npm test`
---
+++ src/tokenizer.ts
@@ -0,0 +1,89 @@
+interface Token {
+  type: string;
+  value: string;
+}
+
+function tokenizer(input: string): Token[] {
+  let current = 0;
+  let tokens: Token[] = [];
+
+  while (current < input.length) {
+    let char = input[current];
+
+    if (char === '(' || char === ')') {
+      tokens.push({
+        type: 'paren',
+        value: char,
+      });
+
+      current += 1;
+      continue;
+    }
+
+    let WHITESPACE = /\s/;
+    if (WHITESPACE.test(char)) {
+      current += 1;
+      continue;
+    }
+
+    const NUMBERS = /[0-9]/;
+    if (NUMBERS.test(char)) {
+      let value = '';
+
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({
+        type: 'number',
+        value,
+      });
+
+      continue;
+    }
+
+    if (char === '"') {
+      let value = '';
+
+      char = input[++current];
+
+      while (char !== '"') {
+        value += char;
+        char = input[++current];
+      }
+
+      char = input[++current];
+
+      tokens.push({
+        type: 'string',
+        value,
+      });
+
+      continue;
+    }
+
+    const LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)) {
+      let value = '';
+
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({
+        type: 'name',
+        value,
+      });
+
+      continue;
+    }
+
+    throw new TypeError(`I don't know what this character is: ${char}`);
+  }
+
+  return tokens;
+}
+
+export default tokenizer;
